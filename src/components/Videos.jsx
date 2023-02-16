f97f47d483d4f10421bf45507db108d0f99bb85b
tomchoe <doodeitstom@yahoo.com>
2023-02-15 21:53:56 -0600

    implemented API, Sidebar, Stying and working on video component

---
+++ src/components/Videos.jsx
@@ -0,0 +1,18 @@
+import { Stack, Box } from '@mui/material';
+import { ChannelCard, VideoCard } from './';
+
+const Videos = ({ videos }) => {
+  console.log(' videos array ', videos);
+  return (
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2} >
+      {videos.map((vid, i) => (
+        <Box key={i}>
+          {vid.id.videoId && <VideoCard video={vid} />}
+          {/* {vid.id.channelId && <ChannelCard channelDetail={vid} />} */}
+        </Box>
+      ))}
+    </Stack>
+  )
+}
+
+export default Videos
